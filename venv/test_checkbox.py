f857d1f84b0e6ee13d7ae54a8c34aa2a7bc138c9
visurm76 <visurm76@gmail.com>
2022-01-22 10:16:53 +0300

    used for search elements 'find element By'

---
+++ venv/test_checkbox.py
@@ -0,0 +1,18 @@
+import time
+import pytest
+#from selenium import webdriver
+from selenium.webdriver.common.by import By
+import utils as u
+
+LINK = u.url['checkbox']
+
+
+def test_checkbox(browser):
+    browser.get(LINK)
+    time.sleep(5)
+    option_check_one = browser.find_element(By.CSS_SELECTOR,"#checkboxes > input[type=checkbox]:nth-child(1)").click()
+    time.sleep(5)
+    option_check_tow = browser.find_element(By.CSS_SELECTOR,"#checkboxes > input[type=checkbox]:nth-child(3)").click()
+    time.sleep(5)
+
+
